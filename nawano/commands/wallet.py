bf212ac0cfb38aa898b248c1cb0e1ede6eb27007
rbw0 <rbw@vault13.org>
2018-09-14 17:32:40 +0200

    enables setting wallet representative

--- nawano/commands/wallet.py
+++ nawano/commands/wallet.py
@@ -4,7 +4,7 @@ import click
 from prompt_toolkit import prompt
 from sys import stdout
 
-from nawano.services import wallet_service, state_service
+from nawano.services import wallet_service, state_service, rep_service
 from nawano.utils import password_input
 from nawano.status import with_status
 from nawano.exceptions import NoSuchWallet, NawanoError
@@ -15,6 +15,24 @@ from .root import root_group
 def _wallet_set_active(wallet_id):
     state_service.set_wallet(wallet_id)
     msg = 'active wallet: {0}'.format(state_service.wallet.name)
+    return None, msg
+
+
+@with_status(text='setting representative')
+def _wallet_set_representative(address):
+    if not state_service.network.validate_address(address):
+        raise NawanoError('invalid address format')
+
+    wallet_id = state_service.wallet.id
+    wr = {
+        'representative_address': address
+    }
+
+    rep = rep_service.get_one(address=address)
+    wr['representative_alias'] = rep.alias if rep else None
+    wallet_service.update(wallet_id, **wr)
+
+    msg = 'representative: {0}'.format(state_service.wallet.representative_address)
     return None, msg
 
 
@@ -30,6 +48,20 @@ def _validate_wallet_name(ctx, param, value):
         raise NawanoError('a wallet with that name already exists')
 
     return wallet_service.validate_name(value)
+
+
+def _validate_rep_alias(ctx, param, value):
+    if not rep_service.get_one(alias=value):
+        raise NawanoError('invalid representative alias provided')
+
+    return value
+
+
+def _validate_rep_address(ctx, param, value):
+    if not state_service.network.validate_address(value):
+        raise NawanoError('invalid address format')
+
+    return value
 
 
 @root_group.group('wallet', short_help='wallet operations')
@@ -62,9 +94,9 @@ def wallet_show(**kwargs):
 
 
 @wallet_group.command('representative', short_help='set representative for accounts in this wallet')
-@click.argument('name', required=True)
-def wallet_set_representative(**kwargs):
-    stdout.write('hello')
+@click.argument('address', required=True)
+def wallet_set_representative(address):
+    return _wallet_set_representative(address)
 
 
 @wallet_group.command('list', short_help='wallet list')
