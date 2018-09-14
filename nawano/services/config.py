86c1f022306d236b087d5321f6a53d0ce6a038a6
rbw0 <rbw@vault13.org>
2018-09-14 17:34:33 +0200

    adds pending_check and weight_check

--- nawano/services/config.py
+++ nawano/services/config.py
@@ -5,9 +5,11 @@ from nawano.exceptions import NawanoError
 
 from ._base import NawanoService
 
-PENDING_COOLDOWN = 'pending_cooldown'
-BALANCE_SYNC_INTERVAL = 'balance_sync_interval'
-REPRESENTATIVE_SYNC_INTERVAL = 'rep_sync_interval'
+PENDING_CHECK_INTERVAL = 'pending_check'
+WEIGHT_CHECK_INTERVAL = 'weight_check'
+BALANCE_REFRESH_INTERVAL = 'balance_refresh'
+REPS_REFRESH_INTERVAL = 'reps_refresh'
+REP_MAX_WEIGHT = 'max_weight'
 RPC_BACKEND = 'backend'
 
 
@@ -47,34 +49,50 @@ class ConfigService(NawanoService):
         self.__state__.backend_refresh()
 
     @property
-    def pending_cooldown(self):
-        return int(self.get_attribute(PENDING_COOLDOWN).value)
+    def max_weight(self):
+        return float(self.get_attribute(REP_MAX_WEIGHT).value)
 
-    @pending_cooldown.setter
-    def pending_cooldown(self, value):
-        self.__set_valid_attribute(PENDING_COOLDOWN, value=value)
+    @max_weight.setter
+    def max_weight(self, value):
+        self.__set_valid_attribute(REP_MAX_WEIGHT, value=value)
 
     @property
-    def rep_sync_interval(self):
-        return int(self.get_attribute(REPRESENTATIVE_SYNC_INTERVAL).value)
+    def pending_check_interval(self):
+        return int(self.get_attribute(PENDING_CHECK_INTERVAL).value)
 
-    @rep_sync_interval.setter
-    def rep_sync_interval(self, value):
+    @pending_check_interval.setter
+    def pending_check_interval(self, value):
+        self.__set_valid_attribute(PENDING_CHECK_INTERVAL, value=value)
+
+    @property
+    def rep_check_interval(self):
+        return int(self.get_attribute(WEIGHT_CHECK_INTERVAL).value)
+
+    @rep_check_interval.setter
+    def rep_check_interval(self, value):
+        self.__set_valid_attribute(WEIGHT_CHECK_INTERVAL, value=value)
+
+    @property
+    def reps_refresh_interval(self):
+        return int(self.get_attribute(REPS_REFRESH_INTERVAL).value)
+
+    @reps_refresh_interval.setter
+    def reps_refresh_interval(self, value):
         if not 30 <= int(value) <= 360:
-            raise NawanoError('representative sync interval must be between 30 and 360 minutes')
+            raise NawanoError('representative refresh interval must be between 30 and 360 minutes')
 
-        self.__set_valid_attribute(REPRESENTATIVE_SYNC_INTERVAL, value * 60)
+        self.__set_valid_attribute(REPS_REFRESH_INTERVAL, value * 60)
 
     @property
-    def balance_sync_interval(self):
-        return int(self.get_attribute(BALANCE_SYNC_INTERVAL).value)
+    def balance_refresh_interval(self):
+        return int(self.get_attribute(BALANCE_REFRESH_INTERVAL).value)
 
-    @balance_sync_interval.setter
-    def balance_sync_interval(self, value):
+    @balance_refresh_interval.setter
+    def balance_refresh_interval(self, value):
         if not 5 <= int(value) <= 120:
-            raise NawanoError('balance sync interval must be between 5 and 120 seconds')
+            raise NawanoError('balance refresh interval must be between 5 and 120 seconds')
 
-        self.__set_valid_attribute(BALANCE_SYNC_INTERVAL, value)
+        self.__set_valid_attribute(BALANCE_REFRESH_INTERVAL, value)
 
     def __set_valid_attribute(self, name, value):
         return self.update(name, value=value)
