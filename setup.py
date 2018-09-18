d5db30b9f9c629388e944c71312414ae6b746c03
Robert Wikman <rbw@vault13.org>
2018-09-19 00:41:18 +0200

    added install_requires

--- setup.py
+++ setup.py
@@ -13,10 +13,7 @@ def get_version():
                 return ast.parse(line).body[0].value.s
 
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-with io.open('README.rst') as readme:
+with io.open('README.md') as readme:
     setup(
         name='nawano',
         packages=find_packages(),
@@ -28,11 +25,25 @@ with io.open('README.rst') as readme:
             ]
         },
         long_description=readme.read(),
-        install_requires=requirements,
+        install_requires=[
+            'privy==6.0.0',
+            'pyblake2==1.1.2',
+            'passwordmeter==0.1.8',
+            'marshmallow>=2.15.1',
+            'simplejson>=3.11.1',
+            'click>=6.7',
+            'prompt-toolkit>=2.0.4',
+            'sqlalchemy>=1.2.7',
+            'sqlalchemy-utils>=0.33.3',
+            'requests>=2.18.4',
+            'texttable>=1.4.0',
+            'crayons>=0.1.2'  # @TODO - remove
+        ],
         author='Robert Wikman',
         author_email='rbw@vault13.org',
         maintainer='Robert Wikman',
         maintainer_email='rbw@vault13.org',
+        python_requires='>=3.5',
         url='https://github.com/rbw/nawano',
         download_url='https://github.com/rbw/nawano/tarball/%s' % get_version(),
         keywords=['nano', 'cryptocurrency', 'cli', 'wallet'],
